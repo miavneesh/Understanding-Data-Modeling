6172e2b4ec3d5386c41b6b3956d75aacf1de7569
Avneesh Mishra <114160711+miavneesh@users.noreply.github.com>
2024-05-13 13:19:53 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -2,4 +2,4 @@ import mongoose from "mongoose";
 
 const userTodo = new mongoose.Schema({},{timestamps:true})
 
-export const Todo = mongoose.model("todo",userTodo)
+export const Todo = mongoose.model("Todo",userTodo)
