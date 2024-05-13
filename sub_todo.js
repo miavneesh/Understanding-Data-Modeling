2558b49d4c653fdd592694239ac0a7cf21034072
Avneesh Mishra <114160711+miavneesh@users.noreply.github.com>
2024-05-13 13:19:37 +0530

    Update sub_todo.js

--- sub_todo.js
+++ sub_todo.js
@@ -2,4 +2,4 @@ import mongoose from "mongoose";
 
 cosnt userSubTodo = new mongoose.Schema({}, {timestamps:true});
 
-export new SubTodo = mongoose.model("Subtodo",userSubTodo)
+export new Subtodo = mongoose.model("Subtodo",userSubTodo)
